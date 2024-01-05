99778310f5efa507a73ae5976b89223aa8526568
aleks <aprokhorenkodev@gmail.com>
2024-01-05 13:09:39 +0100

    We add the use of controllers to the project and link it with views to later be able to work with data

--- Framework-Laravel/myfirstapp/routes/web.php
+++ Framework-Laravel/myfirstapp/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestingController;
 
 Route::view('/','index')->name('index');
 Route::view('/hello', 'helloWorld')->name('hello');
+Route::get('/prueba', [TestingController::class, 'testingPrueba']);
