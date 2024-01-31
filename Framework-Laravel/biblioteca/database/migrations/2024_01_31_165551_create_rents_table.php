aab8a380b75a0f4a5d7551c2c76da12848b5446c
aleks <aprokhorenkodev@gmail.com>
2024-01-31 21:36:50 +0100

    Project of an application made with Laravel

---
+++ Framework-Laravel/biblioteca/database/migrations/2024_01_31_165551_create_rents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id('rent_id');
+            $table->foreignId('book_id')->constrained('books', 'book_id');
+            $table->foreignId('user_id')->constrained('users', 'user_id');
+            $table->date('loan_date');
+            $table->date('deadline')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rents');
+    }
+};
