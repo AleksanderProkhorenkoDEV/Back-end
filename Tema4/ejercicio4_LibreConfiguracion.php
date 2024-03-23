b7f26726eeac7d6a82c42a93dcf0bfeab641f8bd
aleks <aprokhorenkodev@gmail.com>
2024-03-23 23:06:09 +0100

    New exercise

---
+++ Tema4/ejercicio4_LibreConfiguracion.php
@@ -0,0 +1,11 @@
+<!--
+    Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.
+-->
+<?php
+    $contador = 0;
+    for ($i=320; $i >= 160 ; $i -= 20) { 
+            echo "$i <br>";
+            $contador=0;
+    }
+
+?>
