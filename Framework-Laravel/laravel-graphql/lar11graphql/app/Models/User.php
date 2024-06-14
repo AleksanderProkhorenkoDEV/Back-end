c112d0744fcea0f6bc93b729de71de23a41d4bcc
aleks <aprokhorenkodev@gmail.com>
2024-06-14 20:50:06 +0200

    feat: mutations for the model users create - v1

--- Framework-Laravel/laravel-graphql/lar11graphql/app/Models/User.php
+++ Framework-Laravel/laravel-graphql/lar11graphql/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,5 +44,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /** ----------- SCOPES ----------- */
+
+    public function scopesearchByName($query, $type){
+        return $type != null ? $query->where('name', 'LIKE', $type['name'] . '%') : $query;
     }
 }
